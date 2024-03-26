d2fe977ceacc04cbddae06837e29c416d9844e06
Igor Machado Coelho <igor.machado@gmail.com>
2024-03-26 02:48:31 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -64,7 +64,8 @@ function run() {
     }
 
     startTime = new Date()
-    output = execSync(command, {timeout, env}).toString()
+    // output = execSync(command, {timeout, env}).toString()
+    output = execSync(command, {timeout, env, stdio: 'inherit'}).toString()    
     endTime = new Date()
 
     result = generateResult('pass', testName, command, output, endTime - startTime, maxScore)
